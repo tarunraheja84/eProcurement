3f43576b42e6d67f8112f59fd8eabd3c31f93adb
sahil.kumar6 <sahil.kumar@redbasil.in>
2023-10-17 13:01:26 +0530

    added the secret manager

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -4,12 +4,12 @@ import { NextRequest } from "next/server";
 import { accessSecret } from "@/utils/utils";
 
 const handler = async (req: NextRequest, res: any) => {
-  // const secrets = await Promise.all([
-  //   accessSecret("NEXTAUTH_SECRET"),
-  //   accessSecret("GOOGLE_CLIENT_ID"),
-  //   accessSecret("GOOGLE_CLIENT_SECRET")
-  // ])
-  const [secretId , clientId, clientSecret] = ["FVp+sDiHafss5y9W3hRDyx4lZ9INzzFZoyQ22tCTIEs=", process.env.GOOGLE_CLIENT_ID , process.env.GOOGLE_CLIENT_SECRET]
+  const secrets = await Promise.all([
+    accessSecret("E_PROCUREMENT_APP_NEXTAUTH_SECRET"),
+    accessSecret("GOOGLE_CLIENT_ID"),
+    accessSecret("GOOGLE_CLIENT_SECRET")
+  ])
+  const [secretId , clientId, clientSecret] = secrets
 
   return await NextAuth(req, res, {
     providers: [
