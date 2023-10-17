75d83ca3f505163e71b2c1d2efbfbf08f131483e
ritesh.k3 <ritesh.k@redbasil.in>
2023-10-17 14:24:19 +0530

    added initital prisma setup

---
+++ src/app/api/users/create/route.ts
@@ -0,0 +1,28 @@
+import { NextRequest, NextResponse } from "next/server";
+import prisma from '@/lib/prisma';
+import { Prisma } from "@prisma/client";
+interface Data {
+    name?: string,
+    email: string
+}
+
+export const POST = async (request: NextRequest) => {
+
+    try {
+        const userData: Data = await request.json();
+        const result = await prisma.user.create({ data: userData });
+        return NextResponse.json(result);
+
+    } catch (error: any) {
+        let statusCode = 500; // Default to Internal Server Error
+
+        if (error instanceof Prisma.PrismaClientKnownRequestError) {
+            // Handle specific Prisma errors with appropriate status codes
+            if (error.code === 'P2002') {
+                statusCode = 400; // Bad Request
+            }
+        }
+
+        return new Response(error.message, { status: statusCode });
+    }
+};
