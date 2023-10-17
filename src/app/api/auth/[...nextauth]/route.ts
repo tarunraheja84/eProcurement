4761b586048364b1d57319aaebc5e81c1c655983
Sahil Kumar <sahil.kumar@redbasil.in>
2023-10-17 07:53:46 +0000

    added with google signIn in e-procurement app

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,32 @@
+import GoogleProvider from "next-auth/providers/google";
+import NextAuth from "next-auth"
+import { NextRequest } from "next/server";
+import { accessSecret } from "@/utils/utils";
+
+const handler = async (req: NextRequest, res: any) => {
+  const secrets = await Promise.all([
+    accessSecret("E_PROCUREMENT_APP_NEXTAUTH_SECRET"),
+    accessSecret("GOOGLE_CLIENT_ID"),
+    accessSecret("GOOGLE_CLIENT_SECRET")
+  ])
+  const [secretId , clientId, clientSecret] = secrets
+
+  return await NextAuth(req, res, {
+    providers: [
+      GoogleProvider({
+        clientId: clientId as string,
+        clientSecret: clientSecret as string
+      }),
+    ],
+    secret: secretId,
+    session: {
+        strategy: 'jwt',
+        maxAge: 24 * 60 * 60,
+    },
+    jwt: {
+      secret: secretId,
+      maxAge: 24 * 60 * 60,
+    },
+  })
+}
+export {handler as GET, handler as POST}
