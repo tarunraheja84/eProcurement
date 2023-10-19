036030984eec6f47a15b6c6fd90b94eb6ec98f4a
Sahil Kumar <sahil.kumar@redbasil.in>
2023-10-19 08:01:24 +0000

    Role based Authentication on middleware side

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -2,6 +2,9 @@ import GoogleProvider from "next-auth/providers/google";
 import NextAuth from "next-auth"
 import { NextRequest } from "next/server";
 import { accessSecret } from "@/utils/utils";
+import { logger } from "@/setup/logger";
+import prisma from '@/lib/prisma';
+import { User } from "@prisma/client";
 
 const handler = async (req: NextRequest, res: any) => {
   const secrets = await Promise.all([
@@ -15,7 +18,31 @@ const handler = async (req: NextRequest, res: any) => {
     providers: [
       GoogleProvider({
         clientId: clientId as string,
-        clientSecret: clientSecret as string
+        clientSecret: clientSecret as string,
+        async profile(profile) {
+          let userData: any = {
+            name: profile.name,
+            email: profile.email,
+            role: 'USER',
+          };
+          let id = profile.id;
+          let user : User | null;
+          try {
+            user = await prisma.user.findUnique({ // check if user already present 
+              where : {
+                  email : userData.email,
+              },
+            })
+            if (!user) {
+              user = await prisma.user.create({ data: userData }); // if user not exist create user with default "USER" role
+            }
+            id = user.userId;
+            userData.role = user.role;
+          } catch (error) {
+            logger.error(`Error creating user  : ${error}`);
+          }
+          return { role: userData.role , id: profile.sub , ...profile}
+        }
       }),
     ],
     secret: secretId,
@@ -27,6 +54,15 @@ const handler = async (req: NextRequest, res: any) => {
       secret: secretId,
       maxAge: 24 * 60 * 60,
     },
+    callbacks :{
+      async jwt({token, user}){
+        return { ...token, ...user}
+      },
+      async session({session ,token, user}){
+        session.user = token;
+        return session;
+      }
+    }
   })
 }
 export {handler as GET, handler as POST}
---
+++ src/types/user.ts
@@ -0,0 +1,12 @@
+import { UserRole } from "./enums";
+
+export type User  = {
+    userId?: string | null;
+    name?: string | null;
+    email: string;
+    role: UserRole;
+    phoneNumber?: string | null;
+    vendorId?: string | null;
+}
+
+
