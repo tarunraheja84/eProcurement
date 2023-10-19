a57e77a4ec704d1637442518b4ace49be910d7db
Tarun Raheja <tarun.r@redbasil.in>
2023-10-19 14:33:15 +0000

    Procurement Page added

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,11 +4,11 @@ import { NextResponse } from "next/server";
 export default withAuth(
   async function middleware(req: NextRequestWithAuth) {
 
-    if (req.nextauth.token?.role === "USER" && (req.nextUrl.pathname.startsWith('/quotations') || req.nextUrl.pathname.startsWith('/vendors')) ) {
-    // alow access the user's role and route to authorize access accordingly
-    } else {
-    return NextResponse.rewrite(new URL('/access_denied', req.url));
-    }
+    // if (req.nextauth.token?.role === "USER" && (req.nextUrl.pathname.startsWith('/quotations') || req.nextUrl.pathname.startsWith('/vendors') || req.nextUrl.pathname.startsWith('/procurements')|| req.nextUrl.pathname.startsWith('/api')) ) {
+    // // alow access the user's role and route to authorize access accordingly
+    // } else {
+    // return NextResponse.rewrite(new URL('/access_denied', req.url));
+    // }
   },
   {
     callbacks: {
