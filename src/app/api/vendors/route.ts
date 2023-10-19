2f9d70cbf74dfc3e3b5fd1dfadd72fb75cc33ceb
ritesh.k3 <ritesh.k@redbasil.in>
2023-10-19 12:39:48 +0530

    updated vendor creation flow

--- src/app/api/vendors/route.ts
+++ src/app/api/vendors/route.ts
@@ -5,7 +5,7 @@ import { Prisma, Vendor } from "@prisma/client";
 export const POST = async (request: NextRequest) => {
     try {
         const vendorData: Vendor = await request.json();
-        const result = await prisma.vendor.create({ data: vendorData });
+        const result = await prisma.vendor.create({ data: {businessName: vendorData.businessName, businessBrandName: vendorData.businessBrandName, gstin: vendorData.gstin, pan: vendorData.pan, addressLine: vendorData.addressLine, city: vendorData.city, state: vendorData.state, pinCode: vendorData.pinCode, phoneNumber: `+91${vendorData.phoneNumber}`, countryCode: vendorData.countryCode, status: vendorData.status, createdBy: vendorData.createdBy, updatedBy: vendorData.updatedBy } });
         return NextResponse.json(result);
 
     } catch (error: any) {
