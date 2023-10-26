666003d99ce2dc56f240e615e8bb8dadd4809efd
sahil.kumar6 <sahil.kumar@redbasil.in>
2023-10-26 11:52:18 +0530

    Added the facebook , apple , and google authantication for user but need to add the logic

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,4 +1,6 @@
-import GoogleProvider from "next-auth/providers/google";
+import FacebookProvider from 'next-auth/providers/facebook'
+import GoogleProvider from 'next-auth/providers/google'
+import AppleProvider from 'next-auth/providers/apple'
 import NextAuth from "next-auth"
 import { NextRequest } from "next/server";
 import { accessSecret } from "@/utils/utils";
@@ -11,15 +13,28 @@ const handler = async (req: NextRequest, res: any) => {
   const secrets = await Promise.all([
     accessSecret("E_PROCUREMENT_APP_NEXTAUTH_SECRET"),
     accessSecret("GOOGLE_CLIENT_ID"),
-    accessSecret("GOOGLE_CLIENT_SECRET")
+    accessSecret("GOOGLE_CLIENT_SECRET"),
+    accessSecret("APPLE_CLIENT_ID"),
+    accessSecret("APPLE_CLIENT_SECRET"),
+    accessSecret("FACEBOOK_CLIENT_ID"),
+    accessSecret("FACEBOOK_CLIENT_SECRET"),
+
   ])
-  const [secretId , clientId, clientSecret] = secrets
+  const [secretId, googleClientId, googleClientSecret, appleClientId, appleClientSecret, facebookClientId, facebookClientSecret] = secrets
 
   return await NextAuth(req, res, {
     providers: [
+      AppleProvider({
+        clientId: appleClientId as string,
+        clientSecret: appleClientSecret as string
+      }),
+      FacebookProvider({
+        clientId: facebookClientId as string,
+        clientSecret: facebookClientSecret as string
+      }),
       GoogleProvider({
-        clientId: clientId as string,
-        clientSecret: clientSecret as string,
+        clientId: googleClientId as string,
+        clientSecret: googleClientSecret as string,
         async profile(profile) {
           let userData: any = {
             name: profile.name,
@@ -27,11 +42,11 @@ const handler = async (req: NextRequest, res: any) => {
             role: UserRole.USER,
           };
           let id = profile.id;
-          let user :  InternalUser | null;
+          let user: InternalUser | null;
           try {
             user = await prisma.internalUser.findUnique({ // check if user already present 
-              where : {
-                  email : userData.email,
+              where: {
+                email: userData.email,
               },
             })
             if (!user) {
@@ -42,28 +57,29 @@ const handler = async (req: NextRequest, res: any) => {
           } catch (error) {
             logger.error(`Error creating user  : ${error}`);
           }
-          return { role: userData.role , id: profile.sub , ...profile}
+          return { role: userData.role, id: profile.sub, ...profile }
         }
       }),
     ],
+
     secret: secretId,
     session: {
-        strategy: 'jwt',
-        maxAge: 24 * 60 * 60,
+      strategy: 'jwt',
+      maxAge: 24 * 60 * 60,
     },
     jwt: {
       secret: secretId,
       maxAge: 24 * 60 * 60,
     },
-    callbacks :{
-      async jwt({token, user}){
-        return { ...token, ...user}
+    callbacks: {
+      async jwt({ token, user }) {
+        return { ...token, ...user }
       },
-      async session({session ,token, user}){
+      async session({ session, token, user }) {
         session.user = token;
         return session;
       }
     }
   })
 }
-export {handler as GET, handler as POST}
+export { handler as GET, handler as POST }
