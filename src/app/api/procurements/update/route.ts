73d639e506bad4b3e2c04e31fd6e7d1fe4ce7cb5
tarun.r1 <tarun.@redbasil.in>
2023-11-16 16:32:26 +0530

    next commit

--- src/app/api/procurements/update/route.ts
+++ src/app/api/procurements/update/route.ts
@@ -5,15 +5,25 @@ import { Prisma } from "@prisma/client";
 
 export const PATCH = async (request: NextRequest) => {
   try {
-    const data=await request.json();
-    const procurementId=data.procurementId
-    delete data.procurementId;
+    const procurementPlan=await request.json();
+    const procurementId=procurementPlan.procurementId
+    delete procurementPlan.procurementId;
+
+    for(const product of procurementPlan.products.create){
+      delete product.quantity;
+  }
+
+    if(procurementPlan.products.delete){
+      for(const product of procurementPlan.products.delete){
+          delete product.quantity;
+      }
+  }
 
     const result=await prisma.procurement.update({
       where:{
         procurementId:procurementId
       },
-      data:data
+      data:procurementPlan
     })
     return NextResponse.json(result);
   } catch (error: any) {
