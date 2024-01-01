4266c0897f914302409da6c1728c3691529da3a7
sahil.kumar6 <sahil.kumar@redbasil.in>
2024-01-01 13:13:18 +0530

    updated

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,11 +8,9 @@ export default withAuth(
     if (!req.nextauth.token || !req.nextauth.token.status || req.nextauth.token.status === UserStatus.INACTIVE) {
       return NextResponse.rewrite(new URL('/access_denied', req.url));
     }
-    if (req.nextauth.token.userType === UserType.VENDOR_USER && req.nextUrl.pathname.endsWith('/manage_users/create')){
+    if (req.nextUrl.pathname === '/' || (req.nextauth.token.userType === UserType.VENDOR_USER && req.nextUrl.pathname.endsWith('/manage_users/create'))){
       return NextResponse.next()
     }
-    if (req.nextUrl.pathname === '/') return NextResponse.next() 
-    
     if (
       (req.nextauth.token.userType === UserType.VENDOR_USER && !req.nextUrl.pathname.startsWith('/vendor')) ||
       (req.nextauth.token.userType === UserType.INTERNAL_USER && req.nextUrl.pathname.startsWith('/vendor'))
